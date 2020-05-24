9969e983daf59444eb3c012632d0e6c3b3c33db2
Jongbin Chae <kalibianw@gmail.com>
2020-05-24 19:56:06 +0900

    After mfcc

--- Voice similarity/YouTube_Downloader.py
+++ Voice similarity/YouTube_Downloader.py
@@ -1,9 +1,17 @@
 from utils import data_utils as du
+import numpy as np
 
-
-yt_url = "https://www.youtube.com/watch?v=zr-s47f-DOA"
-download_dir = "./data/"
-
+# yt_url = "https://www.youtube.com/watch?v=zr-s47f-DOA"
+# download_dir = "./data/"
+#
 # audio_path = du.download_and_convert(yt_url, download_dir=download_dir)
-audio_path = r"C:\Users\admin\Documents\git_clone\AI\Voice similarity\data\1.wav"
-du.wav_cutter(audio_path=audio_path, output_dir=download_dir + "1/")
+audio_path = r"C:\Users\admin\Documents\git_clone\AI\Voice similarity\data\1\1-5.wav"
+# du.wav_cutter(audio_path=audio_path, output_dir=download_dir + "1/")
+
+norm_S, sr, mfccs_40, mfccs_80 = du.audio_to_mfcc(audio_path=audio_path)
+print("norm_S: ", norm_S)
+print("sr: ", sr)
+print("norm_S's shape: ", np.shape(norm_S))
+print("mfccs_20's shape: ", np.shape(mfccs_40))
+print("mfccs_40's shape: ", np.shape(mfccs_80))
+print(mfccs_40)
--- Voice similarity/utils/data_utils.py
+++ Voice similarity/utils/data_utils.py
@@ -1,7 +1,11 @@
 from pydub.utils import mediainfo
+import matplotlib.pyplot as plt
+import librosa.display
+import numpy as np
 import librosa
 import pytube
 import pydub
+import time
 
 
 def download_and_convert(url, download_dir):
@@ -12,7 +16,6 @@ def download_and_convert(url, download_dir):
 
     tmp = pydub.AudioSegment.from_file(file=download_path, format="mp4")
     tmp.export(out_f=rename_path, format="wav")
-    # os.remove(download_path)
 
     return rename_path
 
@@ -39,3 +42,44 @@ def wav_cutter(audio_path, output_dir):
         index += 1
         t1 = t2
         t2 = t2 + int(cut)
+
+
+def audio_to_mfcc(audio_path):
+    y, sr = librosa.load(path=audio_path, sr=int(mediainfo(audio_path)['sample_rate']))
+
+    S = librosa.feature.melspectrogram(y=y, sr=sr)
+    log_S = librosa.amplitude_to_db(S=S)
+
+    librosa.display.specshow(librosa.power_to_db(S, ref=np.max), sr=sr, x_axis='time', y_axis='mel')
+    plt.title('mel power spectrogram')
+    plt.colorbar(format='%+02.0f dB')
+    plt.show()
+    time.sleep(0.3)
+
+    min_level_db = -100
+
+    def _normalize(S):
+        return np.clip((S - min_level_db) / -min_level_db, 0, 1)
+
+    norm_S = _normalize(log_S)
+
+    librosa.display.specshow(norm_S, sr=sr, x_axis='time', y_axis='mel')
+    plt.title('norm mel power spectrogram')
+    plt.colorbar(format='%+0.1f dB')
+    plt.show()
+    time.sleep(0.3)
+
+    mfccs_40 = librosa.feature.mfcc(y=y, sr=sr, n_mfcc=40)
+    mfccs_80 = librosa.feature.mfcc(y=y, sr=sr, n_mfcc=80)
+
+    librosa.display.specshow(mfccs_40, x_axis="time")
+    plt.title("MFCC with n_mfcc=40")
+    plt.show()
+    time.sleep(0.3)
+
+    librosa.display.specshow(mfccs_80, x_axis="time")
+    plt.title("MFCC with n_mfcc=80")
+    plt.show()
+    time.sleep(0.3)
+
+    return norm_S, sr, mfccs_40, mfccs_80
